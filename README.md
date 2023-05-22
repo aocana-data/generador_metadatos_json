443478d8fa13749ccd294237f29471c7a6eda76a
aocana-data <126675579+aocana-data@users.noreply.github.com>
2023-05-22 14:40:26 +0000

    sample

--- README.md
+++
@@ -1,5 +0,0 @@
-GENERADOR DE METADATOS
----
-
-
-
--- generador_metadatos_json.py
+++ generador_metadatos_json.py
@@ -5,10 +5,13 @@ import json
 
 def GenerarJson(archivo):
 	columnas=[]
-	with open(archivo, 'r') as f:
-		d_reader = csv.DictReader(f)
+	with open(archivo, 'r',encoding='latin-1') as f:
+		#solo para este caso
+		d_reader = csv.DictReader(f,delimiter=';')
+		
 		columnas = d_reader.fieldnames
 		row1 = next(d_reader)
+
 	dic={"recurso":{
 			"displayName":archivo,
 			"description":" ",
@@ -18,20 +21,23 @@ def GenerarJson(archivo):
 			"name":" "
 			}
 		}
+	
 	listaTipos=[]
+
 	for i in columnas:
 		dic['recurso']['attributesDescription'].append({"title": i,"description":i, "type": "string"})
+
 	return dic
 
 
 listaArchivos=os.listdir()
 listaArchivos.remove('generador_metadatos_json.py')
 listaArchivos.remove('.git')
+
 print(listaArchivos)
+
 for archivo in listaArchivos:
 	myjson=GenerarJson(archivo)
-	x = open ("metadata_"+archivo+".json", "w")
+	x = open("metadata_"+archivo+".json", "w",encoding='latin-1')
 	x.write(json.dumps(myjson, indent=3))
 	x.close()
-	
-
